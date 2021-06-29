686a2f7ef3dfbc58e4b21ff5bee60370fa958dcd
xrp <zeros.code7@gmail.com>
2021-06-29 17:51:33 +0900

    210629강의

--- python_refresh/Zip.py
+++ python_refresh/Zip.py
@@ -1,13 +1,15 @@
-# 두개의 list값을 병렬적으로 추출
-aa = ['a1','a2','a3']
-bb = ['b1','b2','b3']
+# # 두개의 list값을 병렬적으로 추출
+# aa = ['a1','a2','a3']
+# bb = ['b1','b2','b3']
+#
+# for i,j in zip(aa,bb):
+#     print(i,j)
+#
+# for i,(a,b) in enumerate(zip(aa,bb)):
+#     print(i,a,b) # i=index, a,b
+#
+# a,b,c = zip((1,2,3),(10,20,30),(100,200,300))
+# print(a,b,c) # zip을 이용해 같은 index 끼리 묶음
 
-for i,j in zip(aa,bb):
-    print(i,j)
-
-for i,(a,b) in enumerate(zip(aa,bb)):
-    print(i,a,b) # i=index, a,b
-
-a,b,c = zip((1,2,3),(10,20,30),(100,200,300))
-print(a,b,c) # zip을 이용해 같은 index 끼리 묶음
-
+a,b = input().split(':')
+print(a,b, sep=':')
